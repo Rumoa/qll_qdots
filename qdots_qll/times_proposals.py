5ad410f2da5c097622e78e7d1e489b6db8edf36b
Antonio <physrumoa@gmail.com>
2024-01-04 16:55:32 +0000

    new times_proposal file with fim maximization

---
+++ qdots_qll/times_proposals.py
@@ -0,0 +1,47 @@
+import jax
+import jax.numpy as jnp
+import numpy as np
+from jax import jit, vmap
+
+
+@jit
+def maximize_fim_time(t, particle, model, alpha):
+    derivative_fim_to_t = jax.grad(
+        (lambda par, t: jnp.linalg.norm(model.fim(par, t))), 1
+    )
+
+    new_t = t
+
+    grad_t = derivative_fim_to_t(particle, new_t)
+
+    new_t = new_t + alpha * grad_t
+
+    def body_fun_fori_loop(i, new_t):
+        grad_t = derivative_fim_to_t(particle, new_t)
+        new_t = new_t + alpha * grad_t
+        return new_t
+
+    return jax.lax.fori_loop(0, 20, body_fun_fori_loop, new_t)
+
+
+@jit
+def fim_time_generator(key, estimated_particles, model):
+    no_candidates = 10
+    key, subkey = jax.random.split(key)
+
+    candidates = jax.random.uniform(
+        subkey,
+        shape=(no_candidates,),
+        minval=0.01,
+        maxval=40.0,
+    )
+
+    times = vmap(maximize_fim_time, in_axes=(0, None, None, None))(
+        candidates, estimated_particles, model, 0.5
+    )
+
+    norm_fims = vmap(
+        lambda time: jnp.linalg.norm(model.fim(estimated_particles, time)),
+        in_axes=(0),
+    )(times)
+    return key, times[np.argmax(norm_fims)]
