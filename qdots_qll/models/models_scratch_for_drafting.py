0ceef8dbdb6bbdad1300e33d7eaad32c7b2084de
toni <physrumoa@gmail.com>
2024-03-27 18:38:29 +0000

    added model 3 params

--- qdots_qll/models/models_scratch_for_drafting.py
+++ qdots_qll/models/models_scratch_for_drafting.py
@@ -273,6 +273,196 @@ class single_qdot(base_class_dimension):
         return qfim
 
 
+class SingleQDot3Params(base_class_dimension):
+    number_of_parameters: int
+    delta: float
+    Omega: float
+    T: float
+    system_hamiltonian: Complex[Array, "d d"]
+    A: Complex[Array, "d d"]
+    POVM_arr: Complex[Array, "no_outcomes d d"]
+    basis_elements: jax.Array
+    # true_parameters: jax.Array
+
+    def __init__(self, POVM_array: Complex[Array, "no_outcomes d d"]):
+        super().__init__(dimension=2)
+        self.number_of_parameters = 3
+        self.delta = 0.12739334807998307
+        self.Omega = 0.5
+        self.T = 30
+        self.system_hamiltonian = self.make_system_hamiltonian()
+        self.A = jnp.array([[1, 0], [0, 0]])
+        self.POVM_arr = POVM_array
+        self.basis_elements = jnp.identity(4)
+
+    @jit
+    def make_system_hamiltonian(self) -> Complex[Array, "d d"]:
+        return jnp.array([[self.delta, self.Omega / 2], [self.Omega / 2, 0]])
+
+    @jit
+    def make_liouvillian(
+        self, particle: Float[Array, "number_of_parameters"]
+    ) -> Complex[Array, "d**2 d**2"]:
+
+        eta = jnp.sqrt(self.Omega**2 + self.delta**2)
+        beta = 1 / (0.130920339126989 * self.T)
+        gp, Sn, Sp = particle
+        gn = jnp.exp(-beta * eta) * gp
+        Snot = -self.delta
+        gnot = (
+            self.positive_eps
+        )  # This is approx to zero. if we write zero derivatives explodes
+        # because this appears in square roots d/dx (sqrt) prop to 1/sqrt
+
+        U = jnp.linalg.eigh(self.system_hamiltonian)[1]
+        Aij = U @ self.A @ dag(U)
+        Cp = 0.5 * gp + 1j * Sp
+        Cn = 0.5 * gn + 1j * Sn
+        Cnot = 0.5 * gnot + 1j * Snot
+
+        Gamma = jnp.array([[Cnot, Cn], [Cp, Cnot]])
+
+        sqrtgamma = jnp.sqrt(jnp.real(Gamma))
+
+        L = jnp.multiply(Aij, sqrtgamma)
+
+        Af = jnp.multiply(Aij, jnp.conjugate(Gamma))
+        Hrenorm = -1j / 2 * (Aij @ dag(Af) - Af @ dag(Aij))
+
+        Htotal = U @ self.system_hamiltonian @ dag(U) + Hrenorm
+        Liouvillian_ebasis = (
+            -1j * (self.spre(Htotal) - self.spost(Htotal))
+            + self.sprepost(dag(L), L)
+            - 0.5 * (self.spre(L @ dag(L)) + self.spost(L @ dag(L)))
+        )
+        return (
+            self.sprepost(U, dag(U))
+            @ Liouvillian_ebasis
+            @ dag(self.sprepost(U, dag(U)))
+        )
+
+    @jit
+    def evolve_initial_state_liouvillian(
+        self,
+        t: Float,
+        liouvillian: Complex[Array, "d**2 d**2"],
+        initial_state: Complex[Array, "d**2"],
+    ) -> Complex[Array, "d**2"]:
+        return expm(t * liouvillian) @ initial_state
+
+    @jit
+    def likelihood_particle(
+        self,
+        particle: Float[Array, "number_of_parameters"],
+        t: Float,
+        initial_state: Complex[Array, "d**2"],
+    ) -> Float[Array, "no_outcomes"]:
+        liouvillian = self.make_liouvillian(particle)
+        evolved_state_vec = self.evolve_initial_state_liouvillian(
+            t, liouvillian, initial_state
+        )
+        evolved_state_mat = (evolved_state_vec).reshape(self.matrix_d.shape)
+        probability = jax.vmap(compute_P_matrix_ver, in_axes=(None, 0))(
+            evolved_state_mat, self.POVM_arr
+        )
+        return probability
+
+    @jit
+    def fim(
+        self,
+        particle: Complex[Array, "number_of_parameters"],
+        t: Float,
+        initial_state: Complex[Array, "d**2"],
+    ) -> Float[Array, "number_of_parameters number_of_parameters"]:
+        p_array = self.likelihood_particle(particle, t, initial_state)
+
+        jacobian = jax.jacobian(self.likelihood_particle, 0)(
+            particle, t, initial_state
+        )
+
+        example_zero_matrix = jnp.zeros([particle.shape[0], particle.shape[0]])
+
+        fim_element = jax.vmap(lambda x, p: jnp.outer(x, x) / p)(
+            jacobian, p_array
+        )
+        return jnp.where(~jnp.isinf(fim_element), fim_element, 0).sum(axis=0)
+
+    @jit
+    def generate_data(
+        self,
+        key: Int[Array, "2"],
+        true_particle: Complex[Array, "number_of_parameters"],
+        t: Float,
+        initial_state: Complex[Array, "d**2"],
+    ) -> Int[Array, "1"]:
+        probabilities = self.likelihood_particle(
+            true_particle, t, initial_state
+        )
+        probabilities = probabilities / probabilities.sum()
+        no_of_outcomes = self.POVM_arr.shape[0]
+        outcome = jax.random.choice(
+            key, a=jnp.arange(no_of_outcomes), p=probabilities
+        )
+        return outcome
+
+    @jit
+    def qfim(
+        self,
+        particle: Complex[Array, "number_of_parameters"],
+        t: Float,
+        initial_state: Complex[Array, "d**2"],
+    ) -> Float[Array, "number_of_parameters number_of_parameters"]:
+
+        d = self.matrix_d.shape[0]
+        no_parameters = particle.shape[0]
+
+        L = self.make_liouvillian(particle)
+        evolved_state = self.evolve_initial_state_liouvillian(
+            t, L, initial_state
+        ).reshape(d, d)
+
+        eigvals, eigvecs = jnp.linalg.eigh(evolved_state)
+
+        partial_rhoi_parameterj = jax.jacobian(
+            lambda particle: self.evolve_initial_state_liouvillian(
+                t, self.make_liouvillian(particle), initial_state
+            ),
+            holomorphic=True,
+        )(particle.astype(jnp.complex64)).T.reshape(
+            no_parameters, d, d
+        )  # first index is related to the parameter
+
+        denominator_lambda_i_plus_lambda_j = jax.vmap(
+            lambda i: jax.vmap(lambda j: 1 / (i + j))(eigvals)
+        )(eigvals)
+
+        braket = jnp.einsum(
+            "im,amn,jn-> ija",
+            jnp.conjugate(eigvecs),
+            partial_rhoi_parameterj,
+            eigvecs,
+        )
+
+        braket_with_denominator_product = jnp.einsum(
+            "ija,  ij-> ija", braket, denominator_lambda_i_plus_lambda_j
+        )
+
+        braket_with_denominator_product_cleaned = jnp.where(
+            ~jnp.isinf(braket_with_denominator_product),
+            braket_with_denominator_product,
+            jnp.zeros(braket_with_denominator_product.shape),
+        )
+
+        qfim = 2 * jnp.real(
+            jnp.einsum(
+                "ija, ijb-> ab",
+                jnp.conjugate(braket),
+                braket_with_denominator_product_cleaned,
+            )
+        )
+        return qfim
+
+
 @jit
 def compute_P_superop(
     evolved_state_vec: Complex[Array, "d**2"],
